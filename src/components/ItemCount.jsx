dce1d4b71ef9fb028adc41aa919e30ea9f065269
Matias Pariente <matiaspariente@hotmail.com>
2022-05-18 13:59:22 -0300

    Sincronizar Counter

---
+++ src/components/ItemCount.jsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+
+const ItemCount = ({ onAdd }) => {
+
+    const [count, setCount] = useState(1);
+
+    const countAdd = () => {
+        setCount(count + 1);
+    };
+
+    const countSubstract = () => {
+        if (count > 1) {
+            setCount(count - 1);
+        }
+    };
+
+
+    return (
+        <>
+            <div>
+                <button onClick={countSubstract} className="btn m-2">-</button>
+                <span>{count}</span>
+                <button onClick={countAdd} className="btn m-2">+</button>
+            </div>
+            <button onClick={() =>onAdd(count)} className="btn">Agregar al carrito</button>
+    </>
+  );
+};
+
+export default ItemCount;
--- src/components/ItemDetail.jsx
+++ src/components/ItemDetail.jsx
@@ -1,9 +1,18 @@
-import { useContext } from "react"
+import { useContext, useState} from "react"
+import { Link } from "react-router-dom"
 import { CartContext } from "../context/CartContext";
+import ItemCount from "./ItemCount";
 
 const ItemDetail = ({item}) => { // funcion que retorna una card con el detalle de un producto
 
+    const [countToAdd,setCountToAdd] = useState(0)
+
     const { addItem } = useContext(CartContext);
+
+    const handleOnAdd = (count) => {
+        setCountToAdd(count);
+        addItem(item,count);
+    }
 
     return (
         <div className="m-20 card-normal border-2 rounded-md w-100 bg-base-100 shadow-xl flex justify-center">
@@ -12,7 +21,13 @@ const ItemDetail = ({item}) => { // funcion que retorna una card con el detalle 
             <h2 className="card-title">{item.title}</h2>
             <p>${item.price}</p>
             <p>{item.detail}</p>
-            <button onClick={() => {addItem(item,1)}} className="btn">Agregar a Carrito</button>
+            {countToAdd === 0 ? (
+                <ItemCount onAdd={handleOnAdd}/>
+            ) : (
+                <Link to="/carrito/">
+                    <button className="btn">Ir al carrito</button>
+                </Link>
+            )}
         </div>
     </div>
     )   
